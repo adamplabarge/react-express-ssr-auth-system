a4803f8aea025327c60241c8478c0c270781f92c
Adam LaBarge <adamlabarge@Adams-MBP.domain>
2017-12-28 12:35:18 -0800

    forms added, cookie set on signup

--- src/app/login.js
+++ src/app/login.js
@@ -6,6 +6,19 @@ export default class Login extends Component {
     return (
       <div>
         <h1>This is the Login Page</h1>
+        <form action="/login" method="post">
+            <div>
+                <label>Username:</label>
+                <input type="text" name="username"/>
+            </div>
+            <div>
+                <label>Password:</label>
+                <input type="password" name="password"/>
+            </div>
+            <div>
+                <input type="submit" value="Log In"/>
+            </div>
+        </form>                  
       </div>
     );
   }
--- src/app/signup.js
+++ src/app/signup.js
@@ -6,6 +6,23 @@ export default class Signup extends Component {
     return (
       <div>
         <h1>This is the Signup Page</h1>
+        <form action="/signup" method="post">
+            <div>
+                <label>Username:</label>
+                <input type="text" name="username"/>
+            </div>
+            <div>
+                <label>Email:</label>
+                <input type="text" name="email"/>
+            </div>    
+            <div>
+                <label>Password:</label>
+                <input type="password" name="password"/>
+            </div>
+            <div>
+                <input type="submit" value="Sign Up"/>
+            </div>
+        </form>                  
       </div>
     );
   }
